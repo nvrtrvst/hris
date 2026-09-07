b1b1646c8011186902028fb4359e67df037c26f0
Reza Muhammad <rezamakbar13@gmail.com>
2026-09-07 13:36:21 +0700

    fix job

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,11 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withSchedule(function (Schedule $schedule) {
-        //$schedule->command('presensi:finalize-alpa')->dailyAt('01:00')->withoutOverlapping();
-        //$schedule->command('presensi:cleanup-foto')->dailyAt('01:30')->withoutOverlapping();
+        // $schedule->command('presensi:finalize-alpa')->dailyAt('01:00')->withoutOverlapping();
+        // $schedule->command('presensi:cleanup-foto')->dailyAt('01:30')->withoutOverlapping();
         // Reminder presensi pagi (06:00) & sebelum jam mulai (10:00) utk yang belum absen
-        //$schedule->command('presensi:reminder')->dailyAt('06:00')->withoutOverlapping();
-        //$schedule->command('presensi:reminder')->dailyAt('10:00')->withoutOverlapping();
+        // $schedule->command('presensi:reminder')->dailyAt('06:00')->withoutOverlapping();
+        // $schedule->command('presensi:reminder')->dailyAt('10:00')->withoutOverlapping();
         // Reminder admin: cek setiap 5 menit
         $schedule->command('reminders:process')->everyFiveMinutes()->withoutOverlapping();
     })
